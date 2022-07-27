8413a40e1c42b7ad01869c31d57a8e37fc181bbc
Oluwafemi Awoyemi <femikola23@gmail.com>
2022-07-27 14:00:56 +0100

    make better

--- src/components/projects.js
+++ src/components/projects.js
@@ -14,6 +14,15 @@ const projects = [
   },
   {
     id: 2,
+    title: 'Forward',
+    description: 'This is a web application shows to the user a collection of articles fetched from an API, the articles can be filtered based on most recent, most viewed, and query search.',
+    tech: ['JavasScript', 'React', 'Ruby On Rails', 'PostgresSQL'],
+    liveLink: 'https://62e127b5ad63ff0b85e27648--timely-cascaron-02b67e.netlify.app/',
+    sourceLink: 'https://github.com/phelian23/forward-frontend',
+    image: rentScoot,
+  },
+  {
+    id: 3,
     title: 'Budget Watch',
     description: 'This is a mobile web application users can manage their budget: have a list of transactions associated with a category, so that the user can see how much money is spent and on what.',
     tech: ['Ruby', 'Rails', 'Css', 'HTML'],
@@ -22,7 +31,7 @@ const projects = [
     image: Budget,
   },
   {
-    id: 3,
+    id: 4,
     title: 'Math Magician',
     description: "Math magicians is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to make simple calculations. and read a random math-related quote.",
     tech: ['ReactJs', 'JavaScript', 'Css'],
@@ -31,7 +40,7 @@ const projects = [
     image: 'https://github.com/phelian23/My-portfolio/blob/main/Images/MathMagic.PNG?raw=true',
   },
   {
-    id: 4,
+    id: 5,
     title: 'Leaderboard',
     description: "This leaderboard website allows scores submitted by different players to be displayed. It also allows you to submit your score. All data is preserved thanks to the external Leaderboard API service.",
     tech: ['html', 'Javascript', 'Css'],
@@ -40,7 +49,7 @@ const projects = [
     image: 'https://github.com/phelian23/My-portfolio/blob/main/Images/Capture1.PNG?raw=true',
   },
   {
-    id: 5,
+    id: 6,
     title: 'Afrobeat Festival',
     description: "This project is a supposed website for the Afro Beats Festival(Imaginary). It consists of two pages and is responsive to mobile and desktop screens.",
     tech: ['html', 'Bootstrap', 'JavaScript'],
@@ -49,7 +58,7 @@ const projects = [
     image: 'https://github.com/phelian23/My-portfolio/blob/main/Images/2021-10-15.png?raw=true',
   },
   {
-    id: 6,
+    id: 7,
     title: 'Camo',
     description: 'Camo Limited is at the forefront of developing and leveraging emerging technologies within the remits of space, communications and quantum technologies. We are committed to the safe and responsible deployment of these technology areas in achieving the sustainable development goals (SDGs).',
     tech: ['HTML', 'React', 'JavaScript', 'CSS'],
@@ -58,7 +67,7 @@ const projects = [
     image: camo,
   },
   {
-    id: 7,
+    id: 8,
     title: 'Movies',
     description: "In this project, we build a movie display site, using the TVmazAPI to get current streaming movies, which allows users to add comments and likes. And using the Microverse involvement API to save data such as likes and comments.",
     tech: ['JavaScript', 'webpack', 'CSS'],
